8f7149101aa59910239bfa9bea7f6371b1d14e74
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-10-12 19:31:38 +0700

    membuat main program dan fungsi print email

--- app/src/main/java/com/example/minggu2/Class.kt
+++ app/src/main/java/com/example/minggu2/Class.kt
@@ -6,4 +6,7 @@ class Contact(val id: Int, var email: String)
 
 fun main() {
     val contact = Contact(1, "mary@gamil.com")
+
+    //prints the value of the property: email
+    println(contact.email)
 }
