728509a102091635aa1f83b0b45e8b1714fb267f
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-10-12 21:21:58 +0700

    Membuat variable neverNull dan check kondisi neverNull

---
+++ app/src/main/java/com/example/minggu2/NullSafety.kt
@@ -0,0 +1,22 @@
+package com.example.minggu2
+
+fun NullSafety() {
+    // neverNull has String type
+    var neverNull: String = "This can't be null"
+
+    // Throws a compiler error
+    // neverNull = null
+
+    //nullable has nullable String type
+    var nullable: String? = "You can keep a null here"
+    // This is OK
+    nullable = null
+
+    // Check for null in Conditions
+    if (neverNull == null) {
+        println("inferredNonNull is Null")
+    }
+    else {
+        println("InfferedNonNull is not Null")
+    }
+}
