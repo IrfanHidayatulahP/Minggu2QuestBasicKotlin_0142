122007b96bc96dfb2692139de35fd82fa0aff924
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-10-12 19:41:19 +0700

    Membuat fungsi dengan parameter string

--- app/src/main/java/com/example/minggu2/Function.kt
+++ app/src/main/java/com/example/minggu2/Function.kt
@@ -1,6 +1,12 @@
 package com.example.minggu2
 
 fun withoutParameter() {
-    println("== withoutParameter")
+    println("== withoutParameter ==")
     println("Hello World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
 }
